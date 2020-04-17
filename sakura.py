6a16bcc9a6e50a06eb53c0ad6c6ebeaac7a7146a
airando <49174057+airando@users.noreply.github.com>
2020-04-18 01:31:09 +0900

    Update sakura.py

--- sakura.py
+++ sakura.py
@@ -1,20 +1,23 @@
 @client.command(aliases=['ui','uinfo'])
 async def userinfo(ctx,name=None):
-    def return_value(content):
-        value = content.replace('online','オンライン').replace('offline','オフライン')
-        value.replace('idle','退席中').replace('dnd','取り込み中')
-value.replace("`create_instant_invite`","`招待リンクを作成`").replace("`kick_members`","`メンバーをキック`").replace("`ban_members`","`メンバーをBan`")
-value.replace("`administrator`","`管理者`").replace("`manage_channels`","`チャンネルの管理`").replace("`manage_guild`","`サーバー管理`")
-value.replace("`add_reactions`","`リアクションの追加`").replace("`view_audit_log`","`サーバーログの表示`").replace("`priority_speaker`","`優先スピーカー`")
-value.replace("`stream`","`不明`").replace("`read_messages`","`メッセージを読む`").replace("`send_messages`","`メッセージを送信`")
-value.replace("`send_tts_messages`","`TTSメッセージを送信`").replace("`manage_messages`","`メッセージの管理`").replace("`embed_links`","`埋め込みリンク`")
-value.replace("`attach_files`","`ファイルの添付`").replace("`read_message_history`","`メッセージ履歴を読む`").replace("`mention_everyone`","`全員宛メンション`")
-value.replace("`external_emojis`","`外部の絵文字の使用`").replace("`view_guild_insights`","``").replace("`connect`","`接続`")
-value.replace("`speak`","`発言`").replace("`mute_members`","`発言`").replace("`mute_members`","`メンバーをミュート`") .replace("`deafen_members`","`メンバーのスピーカーをミュート`")
-value.replace("`move_members`","`メンバーの移動`").replace("`use_voice_activation`","`音声検出を使用`").replace("`change_nickname`","`ニックネームの変更`")
-value.replace("`manage_nicknames`","`ニックネームの管理`").replace("`manage_roles`","`役職の管理`").replace("`manage_webhooks`","`webhookの管理`")
-value.replace("`manage_emojis`","`絵文字の管理`")
-        return value
+    def rv(content):
+        if isinstance(content,str):
+            value = content.replace('online','オンライン').replace('offline','オフライン')
+            value.replace('idle','退席中').replace('dnd','取り込み中')
+            value.replace("`create_instant_invite`","`招待リンクを作成`").replace("`kick_members`","`メンバーをキック`").replace("`ban_members`","`メンバーをBan`")
+            value.replace("`administrator`","`管理者`").replace("`manage_channels`","`チャンネルの管理`").replace("`manage_guild`","`サーバー管理`")
+            value.replace("`add_reactions`","`リアクションの追加`").replace("`view_audit_log`","`サーバーログの表示`").replace("`priority_speaker`","`優先スピーカー`")
+            value.replace("`stream`","`不明`").replace("`read_messages`","`メッセージを読む`").replace("`send_messages`","`メッセージを送信`")
+            value.replace("`send_tts_messages`","`TTSメッセージを送信`").replace("`manage_messages`","`メッセージの管理`").replace("`embed_links`","`埋め込みリンク`")
+            value.replace("`attach_files`","`ファイルの添付`").replace("`read_message_history`","`メッセージ履歴を読む`").replace("`mention_everyone`","`全員宛メンション`")
+            value.replace("`external_emojis`","`外部の絵文字の使用`").replace("`view_guild_insights`","`サーバーインサイトを見る`").replace("`connect`","`接続`")
+            value.replace("`speak`","`発言`").replace("`mute_members`","`発言`").replace("`mute_members`","`メンバーをミュート`") .replace("`deafen_members`","`メンバーのスピーカーをミュート`")
+            value.replace("`move_members`","`メンバーの移動`").replace("`use_voice_activation`","`音声検出を使用`").replace("`change_nickname`","`ニックネームの変更`")
+            value.replace("`manage_nicknames`","`ニックネームの管理`").replace("`manage_roles`","`役職の管理`").replace("`manage_webhooks`","`webhookの管理`")
+            value.replace("`manage_emojis`","`絵文字の管理`")
+            return value
+        elif content is None:
+            return 'なし'
 
     if name is None:member = ctx.author
     else:
@@ -38,15 +41,21 @@ value.replace("`manage_emojis`","`絵文字の管理`")
     embed.add_field(name="ID",value=str(user.id))
     embed.add_field(name="ボット",value=str(user.bot))
     if member is not None:
-        embed.add_field(name="ステータス",value=str(member.status),inline=False)
+        embed.add_field(name="ステータス",value=rv(member.status),inline=False)
         if member.activities:
+            activities = []
+            for activity,count in zip(member.activities,range(len(member.activities))):
+                content = f'[`{count}`]'
+                if activity.type is discord.ActivityType.custom:
+                    content += f`{activity.emoji}`
             embed.add_field(name=f"アクティビティ({len(member.activities)})",value="\n".join(c.name for c in member.activities))
-        else:embed.add_field(name="アクティビティ(0)",value="None")
+        else:embed.add_field(name="アクティビティ(0)",value=rv(None))
         embed.add_field(name="アカウント作成時刻",value=member.created_at.strftime("%F %T"))
     if member_:
         embed.add_field(name="サーバー参加時刻",value=member_.joined_at.strftime("%F %T"))
-        embed.add_field(name="ニックネーム",value=str(member_.nick))
+        embed.add_field(name="ニックネーム",value=rv(member_.nick))
         embed.add_field(name=f"役職({len(member_.roles)})",value=','.join(c.mention for c in list(reversed(member_.roles))[:20:]))
         pers = [f"`{c}`" for c in dict(member_.guild_permissions) if dict(member_.guild_permissions)[c] is True]
-        embed.add_field(name=f"権限({len(pers)})",value=",".join(pers))
+        embed.add_field(name=f"権限({len(pers)})",value=rv(",".join(pers)))
+        color = member_.color
     await ctx.send(embed=embed)
