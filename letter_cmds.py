a0fd87e9d2462b58cab1675f61e66ba91489de3d
Tony / _toni <49174057+airando@users.noreply.github.com>
2020-06-27 23:21:30 +0900

    Update letter_cmds.py

--- letter_cmds.py
+++ letter_cmds.py
@@ -1,1 +1,43 @@
+"""
+大文字小文字を区別させずにコマンドを呼び出すコード
+"""
+
+"""
+===================================
+"""
+#Cogではない場合
+
+async def process_commands(message):
+    if message.author.bot:return
+    ctx = await bot.get_context(message)
+    if ctx.prefix is not None:
+        ctx.command = self.all_commands.get(ctx.invoked_with.lower())
+        await bot.invoke(ctx)
+
+@bot.event
+async def on_message(message):
+    await process_commands(message)
+
+"""
+===================================
+"""
+#Cogの場合
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix="::")
+
+    async def process_commands(self, message):
+        if message.author.bot:return
+        ctx = await self.get_context(message)
+        if ctx.prefix is not None:
+            ctx.command = self.all_commands.get(ctx.invoked_with.lower())
+            await self.invoke(ctx)
+
+    async def on_message(self,message):
+        await self.process_commands(message)
+
+"""
+===================================
+"""
 
